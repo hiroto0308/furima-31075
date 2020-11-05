c67f61a2b372e991fb1162a3df7896f3faf2f4da
hiroto0308 <72369165+hiroto0308@users.noreply.github.com>
2020-11-05 19:30:17 +0900

    バリデーションの変更２

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,10 @@ class User < ApplicationRecord
 
   with_options presence: true do
    validates :nickname               
-   validates :first_name            
-   validates :family_name          
-   validates :first_name_kana      
-   validates :family_name_kana     
+   validates :first_name,        format: { with: /\A[ぁ-んァ-ン一-龥]/ }          
+   validates :family_name,       format: { with: /\A[ぁ-んァ-ン一-龥]/ }        
+   validates :first_name_kana,   format: { with: /\A[ァ-ヶー－]+\z/ }      
+   validates :family_name_kana,  format: { with: /\A[ァ-ヶー－]+\z/ }     
    validates :birthday               
   end
    VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}+\z/i.freeze
