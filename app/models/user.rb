21147626a4bb6300e80a5594f09bef588e4c76ac
hiroto0308 <72369165+hiroto0308@users.noreply.github.com>
2020-11-05 15:46:56 +0900

    RuboCop実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         validates :nickname,               presence: true
-         validates :first_name,             presence: true
-         validates :family_name,            presence: true
-         validates :first_name_kana,        presence: true
-         validates :family_name_kana,       presence: true
-         validates :birthday,               presence: true
-         VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}+\z/i
-         validates :password, format: { with: VALID_PASSWORD_REGEX }
+  validates :nickname,               presence: true
+  validates :first_name,             presence: true
+  validates :family_name,            presence: true
+  validates :first_name_kana,        presence: true
+  validates :family_name_kana,       presence: true
+  validates :birthday,               presence: true
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}+\z/i.freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,65 +6,65 @@ describe User do
 
   describe 'ユーザー新規登録' do
     context '新規登録がうまくいくとき' do
-      it "nickname等全てのカラムが存在すれば登録できる" do
+      it 'nickname等全てのカラムが存在すれば登録できる' do
         expect(@user).to be_valid
       end
-      it "passwordが6文字以上の半角英数字を両方使用であれば登録できる" do
-        @user.password = "aaa000"
-        @user.password_confirmation = "aaa000"
-        expect(@user).to be_valid 
+      it 'passwordが6文字以上の半角英数字を両方使用であれば登録できる' do
+        @user.password = 'aaa000'
+        @user.password_confirmation = 'aaa000'
+        expect(@user).to be_valid
       end
     end
 
     context '新規登録がうまくいかないとき' do
-      it "nicknameが空だと登録できない" do
-        @user.nickname = ""
+      it 'nicknameが空だと登録できない' do
+        @user.nickname = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Nickname can't be blank")
       end
-      it "emailが空では登録できない" do
-        @user.email = ""
+      it 'emailが空では登録できない' do
+        @user.email = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Email can't be blank")
       end
-      it "重複したemailが存在する場合登録できない" do
+      it '重複したemailが存在する場合登録できない' do
         @user.save
         another_user = FactoryBot.build(:user)
         another_user.email = @user.email
         another_user.valid?
-        expect(another_user.errors.full_messages).to include("Email has already been taken")
+        expect(another_user.errors.full_messages).to include('Email has already been taken')
       end
-      it "passwordが空では登録できない" do
-        @user.password = ""
+      it 'passwordが空では登録できない' do
+        @user.password = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Password can't be blank")
       end
-      it "passwordが5文字以下であれば登録できない" do
-        @user.password = "a0000"
-        @user.password_confirmation = "a0000"
+      it 'passwordが5文字以下であれば登録できない' do
+        @user.password = 'a0000'
+        @user.password_confirmation = 'a0000'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
+        expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
       end
-      it "passwordが存在してもpassword_confirmationが空では登録できない" do
-        @user.password_confirmation = ""
+      it 'passwordが存在してもpassword_confirmationが空では登録できない' do
+        @user.password_confirmation = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
       end
-      it "passwordが数字だけで6文字以上だと登録できない" do
-        @user.password = "000000"
-        @user.password_confirmation = "000000"
+      it 'passwordが数字だけで6文字以上だと登録できない' do
+        @user.password = '000000'
+        @user.password_confirmation = '000000'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Password is invalid")
+        expect(@user.errors.full_messages).to include('Password is invalid')
       end
-      it "passwordが英語だけで6文字以上だと登録できない" do
-        @user.password = "000000"
-        @user.password_confirmation = "000000"
+      it 'passwordが英語だけで6文字以上だと登録できない' do
+        @user.password = '000000'
+        @user.password_confirmation = '000000'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Password is invalid")
-      end  
-      it "passwordとpassword_confirmationが不一致では登録できないこと" do
-        @user.password = "aaa000"
-        @user.password_confirmation = "000aaa"
+        expect(@user.errors.full_messages).to include('Password is invalid')
+      end
+      it 'passwordとpassword_confirmationが不一致では登録できないこと' do
+        @user.password = 'aaa000'
+        @user.password_confirmation = '000aaa'
         @user.valid?
         expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
       end
