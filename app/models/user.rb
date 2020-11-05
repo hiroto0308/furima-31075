fa3733c1151170fc6c2085de188e17c69c8a1974
hiroto0308 <72369165+hiroto0308@users.noreply.github.com>
2020-11-05 16:35:08 +0900

    バリデーションの変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname,               presence: true
-  validates :first_name,             presence: true
-  validates :family_name,            presence: true
-  validates :first_name_kana,        presence: true
-  validates :family_name_kana,       presence: true
-  validates :birthday,               presence: true
-  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}+\z/i.freeze
-  validates :password, format: { with: VALID_PASSWORD_REGEX }
+  with_options presence: true do
+   validates :nickname,               
+   validates :first_name,             
+   validates :family_name,            
+   validates :first_name_kana,        
+   validates :family_name_kana,       
+   validates :birthday,               
+  end
+   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}+\z/i.freeze
+   validates :password, format: { with: VALID_PASSWORD_REGEX }
 end
