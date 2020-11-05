1d992322bd440056285fcde749435950b3b73546
hiroto0308 <72369165+hiroto0308@users.noreply.github.com>
2020-11-05 20:35:54 +0900

    Rubocop実行２

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,13 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   with_options presence: true do
-   validates :nickname               
-   validates :first_name,        format: { with: /\A[ぁ-んァ-ン一-龥]/ }          
-   validates :family_name,       format: { with: /\A[ぁ-んァ-ン一-龥]/ }        
-   validates :first_name_kana,   format: { with: /\A[ァ-ヶー－]+\z/ }      
-   validates :family_name_kana,  format: { with: /\A[ァ-ヶー－]+\z/ }     
-   validates :birthday               
+    validates :nickname
+    validates :first_name,        format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+    validates :family_name,       format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+    validates :first_name_kana,   format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :family_name_kana,  format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :birthday
   end
-   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}+\z/i.freeze
-   validates :password, format: { with: VALID_PASSWORD_REGEX }
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}+\z/i.freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX }
 end
