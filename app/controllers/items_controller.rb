b10252804d48df19c55df4e0013c6c9a72c4cf24
hiroto0308 <72369165+hiroto0308@users.noreply.github.com>
2020-11-08 06:33:53 +0900

    商品情報保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,5 +5,20 @@ class ItemsController < ApplicationController
 
   def new
     @item = Item.new
+    
+  end  
+
+  def create
+    @item = Item.create(item_params)
+    if @item.save
+      redirect_to root_path
+    else 
+      render :new
+    end   
+  end  
+
+  private
+  def item_params
+    params.require(:item).permit(:image, :product, :introduction, :category_id, :item_condition_id, :price,  :delivery_fee_id, :delivery_area_id, :delivery_days_id ).merge(user_id: current_user.id)
   end  
 end
