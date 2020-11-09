cda14f97f4275ec3036a3b32a5dde1316f59da88
hiroto0308 <72369165+hiroto0308@users.noreply.github.com>
2020-11-09 11:40:10 +0900

    テストコードの用意

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,15 @@
 FactoryBot.define do
   factory :item do
-    
+    image { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/test.jpg')) }
+    product {"hoge"}
+    introduction {Faker::Lorem.sentence}
+    category_id {2}
+    item_condition_id {2}
+    price {500}
+    delivery_fee_id {2}
+    delivery_area_id {2}
+    delivery_days_id {2}  
+    association :user 
+
   end
 end
