f4bc2bae613ea9eb6ce7d09cf8a11f299a497ee8
hiroto0308 <72369165+hiroto0308@users.noreply.github.com>
2020-11-09 17:09:54 +0900

    Revert "コントローラーの訂正"
    This reverts commit 735591c18fa4f579d922a365aa83dedd6959a93e.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new]
   def index
   end
 
@@ -8,6 +8,7 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.create(item_params)
     if @item.save
       redirect_to root_path
     else
