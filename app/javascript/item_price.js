24dbc46744c73d4c48237fad3995edeabb596d88
hiroto0308 <72369165+hiroto0308@users.noreply.github.com>
2020-11-09 15:51:30 +0900

    金額入力時のjs作成

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,3 +1,15 @@
 window.addEventListener('load', () => {
-  console.log("OK");
+  // 価格、販売手数料、販売利益のデータ取得
+const priceInput = document.getElementById("item-price");
+const addTaxDom = document.getElementById("add-tax-price");
+const salesProfit = document.getElementById("profit");
+priceInput.addEventListener("input", () => {
+  const inputValue = priceInput.value;
+  //  販売手数料の処理
+  var price =  inputValue * 0.1
+  addTaxDom.innerHTML = Math.floor(price);
+  // 販売利益の処理
+  var profit = inputValue * 0.9
+  salesProfit.innerHTML = Math.floor(profit);
+})
 });
