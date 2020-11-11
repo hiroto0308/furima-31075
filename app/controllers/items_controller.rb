9d4295145292c35d4986662560dca94b7b57afaf
hiroto0308 <72369165+hiroto0308@users.noreply.github.com>
2020-11-11 21:28:16 +0900

    出品者のみ削除アクション修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, except: [:index, :new, :create]
+  before_action :login_user, only: [:edit, :destroy]
   def index
     @items = Item.includes(:user).order(created_at: :desc)
   end
@@ -22,7 +23,6 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    redirect_to root_path unless current_user == @item.user_id
   end
 
   def update
@@ -35,7 +35,6 @@ class ItemsController < ApplicationController
 
   def destroy
     @item.destroy
-    redirect_to root_path unless current_user == @item.user_id
   end
 
   private
@@ -46,5 +45,9 @@ class ItemsController < ApplicationController
 
   def set_item
     @item = Item.find(params[:id])
+  end
+
+  def login_user
+    redirect_to root_path unless current_user == @item.user_id
   end
 end
