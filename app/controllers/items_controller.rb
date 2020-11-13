1679d51ed76c63c8a9f2309bfe1082f445f2a049
hiroto0308 <72369165+hiroto0308@users.noreply.github.com>
2020-11-13 15:08:35 +0900

    訂正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -48,6 +48,6 @@ class ItemsController < ApplicationController
   end
 
   def login_user
-    redirect_to root_path unless current_user == @item.user_id
+    redirect_to root_path unless current_user.id == @item.user_id
   end
 end
