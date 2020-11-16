67e232d6aece21aa626c8a70e6a757e3b4d3e903
hiroto0308 <72369165+hiroto0308@users.noreply.github.com>
2020-11-16 19:12:29 +0900

    クレジット決済のコントローラー

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,6 +8,7 @@ class OrdersController < ApplicationController
     @item = Item.find(params[:item_id])
     @donation_form = DonationForm.new(donation_params)   
     if @donation_form.valid?
+      pay_item
       @donation_form.save
       redirect_to root_path
     else
@@ -18,7 +19,15 @@ class OrdersController < ApplicationController
  private
   # 全てのストロングパラメーターを1つに統合
  def donation_params
-  params.require(:donation_form).permit(:post_code, :delivery_area_id, :city, :address, :buliding_name, :phone).merge(user_id: current_user.id, item_id: params[:item_id])
+  params.require(:donation_form).permit(:post_code, :delivery_area_id, :city, :address, :buliding_name, :phone).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
  end 
 
+ def pay_item
+  Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+  Payjp::Charge.create(
+    amount: @item.price,
+    card: donation_params[:token],    
+    currency: 'jpy'                 
+  )
+ end 
 end
