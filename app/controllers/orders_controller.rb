4f6a5b3f5a2e0a5fba6a79a0353c3466a74887c3
hiroto0308 <72369165+hiroto0308@users.noreply.github.com>
2020-11-17 17:46:12 +0900

    商品購入の条件分岐

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,12 +1,14 @@
 class OrdersController < ApplicationController
+  before_action :set_item, only: [:index, :create]
+  before_action :move_to_signed_in, only: [:index]
+  before_action :login_user, only: [:index]
+  before_action :sold_out_item, only: [:index]
   def index
-   @donation_form = DonationForm.new
-   @item = Item.find(params[:item_id])
+    @donation_form = DonationForm.new
   end
-  
+
   def create
-    @item = Item.find(params[:item_id])
-    @donation_form = DonationForm.new(donation_params)   
+    @donation_form = DonationForm.new(donation_params)
     if @donation_form.valid?
       pay_item
       @donation_form.save
@@ -14,20 +16,37 @@ class OrdersController < ApplicationController
     else
       render :index
     end
- end
+  end
 
- private
+  private
+
   # 全てのストロングパラメーターを1つに統合
- def donation_params
-  params.require(:donation_form).permit(:post_code, :delivery_area_id, :city, :address, :buliding_name, :phone).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
- end 
+  def donation_params
+    params.require(:donation_form).permit(:post_code, :delivery_area_id, :city, :address, :buliding_name, :phone).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
 
- def pay_item
-  Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
-  Payjp::Charge.create(
-    amount: @item.price,
-    card: donation_params[:token],    
-    currency: 'jpy'                 
-  )
- end 
+  def pay_item
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: donation_params[:token],
+      currency: 'jpy'
+    )
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def login_user
+    redirect_to root_path if current_user.id == @item.user_id
+  end
+
+  def move_to_signed_in
+    redirect_to '/users/sign_in' unless user_signed_in?
+  end
+
+  def sold_out_item
+    redirect_to root_path if @item.purchase.present?
+  end
 end
